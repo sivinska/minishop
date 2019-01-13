082cd85eb8af538cbe226692cadecf0c70d54234
Kevin LEBON <klebon@e2r1p7.42.fr>
2019-01-13 10:59:54 +0100

    install file working

--- install.php
+++ install.php
@@ -1,21 +1,21 @@
 <?php
 
 // Name of the file
-$filename = 'churc.sql';
+$filename = 'rush00.sql';
 // MySQL host
 $mysql_host = 'localhost';
 // MySQL username
 $mysql_username = 'root';
 // MySQL password
-$mysql_password = '';
+$mysql_password = "keenouxe";
 // Database name
-$mysql_database = 'dump';
+$mysql_database = 'rush00';
 
 // Connect to MySQL server
-mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
-// Select database
-mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
-
+$connection = mysqli_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+		// Select database
+mysqli_query($connection, "CREATE DATABASE IF NOT EXISTS ".$mysql_database);
+mysqli_query($connection, "USE ".$mysql_database);
 // Temporary variable, used to store current query
 $templine = '';
 // Read in entire file
@@ -23,20 +23,21 @@ $lines = file($filename);
 // Loop through each line
 foreach ($lines as $line)
 {
-// Skip it if it's a comment
-if (substr($line, 0, 2) == '--' || $line == '')
-    continue;
+	// Skip it if it's a comment
+	if (substr($line, 0, 2) == '--' || $line == '')
+    	continue;
 
-// Add this line to the current segment
-$templine .= $line;
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // Perform the query
-    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-    // Reset temp variable to empty
-    $templine = '';
+	// Add this line to the current segment
+	$templine .= $line;
+	// If it has a semicolon at the end, it's the end of the query
+	if (substr(trim($line), -1, 1) == ';')
+	{
+    	// Perform the query
+    	mysqli_query($connection, $templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    	// Reset temp variable to empty
+    	$templine = '';
+	}
 }
-}
- echo "Tables imported successfully";
+echo "Tables imported successfully";
+
 ?>
