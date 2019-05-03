9c080d8d4e1cc9daa74a434d9d8c03ba6be711a4
Sarune Ivinskaite <33516726+sivinska@users.noreply.github.com>
2019-05-03 16:27:58 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 
 
 /* Attempt to connect to MySQL database */
-$connection = mysqli_connect("localhost", "root", "12345678", "rush00");
+$connection = mysqli_connect("gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306", "d5pemg5m2sv3lkuy", "gblaszkquv2fxruw", "otpz4163njjx6yae");
 // Check connection
 if($connection === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
